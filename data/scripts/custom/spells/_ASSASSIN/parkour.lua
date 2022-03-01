f4ae228f8adf8c174496ad76233f1754ad3d03fc
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-02-28 22:39:48 -0300

    Update parkour.lua

--- data/scripts/custom/spells/_ASSASSIN/parkour.lua
+++ data/scripts/custom/spells/_ASSASSIN/parkour.lua
@@ -6,7 +6,7 @@ local function levitate(creature, parameter)
 		toPosition:getNextPosition(creature:getDirection())
 
 		local tile = Tile(parameter == "up" and Position(fromPosition.x, fromPosition.y, fromPosition.z - 1) or toPosition)
-		if not tile or not tile:getGround() and not tile:hasFlag(parameter == "up" and TILESTATE_IMMOVABLEBLOCKSOLID or TILESTATE_BLOCKSOLID) then
+		-- if not tile or not tile:getGround() and not tile:hasFlag(parameter == "up" and TILESTATE_IMMOVABLEBLOCKSOLID or TILESTATE_BLOCKSOLID) then
 			tile = Tile(toPosition.x, toPosition.y, toPosition.z + (parameter == "up" and -1 or 1))
 
 			-- if tile and tile:getGround() and not tile:hasFlag(bit.bor(TILESTATE_IMMOVABLEBLOCKSOLID, TILESTATE_FLOORCHANGE)) then

f4d7f2c6665134c60ec152c7e07c9456e4dc02fb
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-03-04 20:02:16 -0300

    Update call_spiritual_beast.lua
    script almost completely rewrited

--- data/scripts/spells/_HUNTER/call_spiritual_beast.lua
+++ data/scripts/spells/_HUNTER/call_spiritual_beast.lua
@@ -30,56 +30,57 @@ function spell.onCastSpell(player, variant)
 			return false
 		end
 	-- end
-
-	local manaCost = monsterType:getManaCost()
-	if player:getMana() < manaCost and not getPlayerFlagValue(player, PlayerFlag_HasInfiniteMana) then
-		player:sendCancelMessage(RETURNVALUE_NOTENOUGHMANA)
-		position:sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
-	
-	if player:getLevel() < 40 then
-		if #player:getSummons() >= 1 then
-			player:sendCancelMessage("Hati ja esta com voce.")
-			position:sendMagicEffect(CONST_ME_POFF)
-			return false
-		end
-	else end
 	
 	-- variaveis do pet scaling
 	local damage_scaling_value = (player:getLevel() * 5) + 100 -- dano do pet aumenta em 5% por level do hunter
 	local damage_scaling = Condition(CONDITION_ATTRIBUTES)
 	damage_scaling:setParameter(CONDITION_PARAM_TICKS, -1)
 	damage_scaling:setParameter(CONDITION_PARAM_BUFF_DAMAGEDEALT, damage_scaling_value)
-
-	local summon = Game.createMonster(monsterName, position, true, false, player)
-
-	if not summon then
-		player:sendCancelMessage(RETURNVALUE_NOTENOUGHROOM)
-		position:sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
-
-	player:addMana(-manaCost)
-	player:addManaSpent(manaCost)
-	position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
-	summon:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	
-	--Vida e buff de dano pro pet, escalando com o Level do Hunter
-	summon:addCondition(damage_scaling)
-	summon:setMaxHealth(player:getMaxHealth() * 2) -- vida do pet = dobro da vida do hunter
-	summon:setHealth(summon:getMaxHealth())
+	if player:getLevel() < 40 then
+		if #player:getSummons() >= 1 then
+			player:sendCancelMessage("Hati ja esta com voce.")
+			position:sendMagicEffect(CONST_ME_POFF)
+			return false
+		else
+			local summon = Game.createMonster(monsterName, position, true, false, player)
+			position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+			summon:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			--Vida e buff de dano pro pet, escalando com o Level do Hunter
+			summon:addCondition(damage_scaling)
+			summon:setMaxHealth(player:getMaxHealth() * 2) -- vida do pet = dobro da vida do hunter
+			summon:setHealth(summon:getMaxHealth())
+		end
+	else end
+
 	
 	-- sumona skoll se o hunter for level 40 ou maior
-	if player:getLevel() >= 40 then 
-		local summon2 = Game.createMonster(monster2Name, position, true, false, player)
-		summon2:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		
+	if player:getLevel() >= 40 then
+	local hati = 0
+	local skoll = 0
+		for _, summon in ipairs(player:getSummons()) do
+			if summon:getName() == "Skoll" then
+				skoll = 1
+			elseif summon:getName() == "Hati" then
+				hati = 1
+			else end
+		end
+		if skoll == 0 then
+			local summon2 = Game.createMonster(monster2Name, position, true, false, player)
+			summon2:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			--Vida e buff de dano pro pet, escalando com o Level do Hunter
-		summon2:addCondition(damage_scaling)
-		summon2:setMaxHealth(player:getMaxHealth())
-		summon2:setHealth(summon2:getMaxHealth())
-		summon:setMaxHealth(player:getMaxHealth()) -- vida do pet = vida do hunter
+			summon2:addCondition(damage_scaling)
+			summon2:setMaxHealth(player:getMaxHealth())
+			summon2:setHealth(summon2:getMaxHealth())
+		else end
+		if hati == 0 then
+			local summon = Game.createMonster(monsterName, position, true, false, player)
+			summon:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			--Vida e buff de dano pro pet, escalando com o Level do Hunter
+			summon:addCondition(damage_scaling)
+			summon:setMaxHealth(player:getMaxHealth()) -- vida do pet = vida do hunter
+			summon:setHealth(summon:getMaxHealth())
+		end
 	else end
 	
 	

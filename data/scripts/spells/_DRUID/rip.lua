10d4cbac53dddfaed965f9f777afb580009918ac
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-03-03 22:54:45 -0300

    Update rip.lua
    applyDot over applyBleeding

--- data/scripts/spells/_DRUID/rip.lua
+++ data/scripts/spells/_DRUID/rip.lua
@@ -5,16 +5,10 @@ combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
 combat:setParameter(COMBAT_PARAM_USECHARGES, 1)
 combat:setArea(createCombatArea(AREA_WAVE6, AREADIAGONAL_WAVE6))
 
-local condition = Condition(CONDITION_BLEEDING) -- sangramento bear
-condition:setParameter(CONDITION_PARAM_DELAYED, 1)
-local condition2 = Condition(CONDITION_BLEEDING) -- sangramento feral
-condition2:setParameter(CONDITION_PARAM_DELAYED, 1)
-
 function onGetFormulaValues(player, skill, attack, factor)
 
 	local min = 0
 	local max = -1
-	local bleedbear = 0
 	
 	local skillTotal = skill * attack
 	local levelTotal = player:getLevel() / 5
@@ -22,21 +16,13 @@ function onGetFormulaValues(player, skill, attack, factor)
 	min = (((skillTotal * 0.04) + 31) + (levelTotal)) / 2
 	max = (((skillTotal * 0.08) + 45) + (levelTotal)) / 2
 		
-	if (player:getStorageValue(Storage_.bear_form) > 0) then -- se for urso, causa metade do dano
-			bleedbear = (((skillTotal * 0.08) + 45) + (levelTotal)) / 4 -- max / 2 , usar a variavel max estava bugando ao transformar em feral
-			condition:addDamage(2, 1000, - bleedbear)
-			condition:addDamage(3, 1000, - bleedbear * 2 / 3)
-			--condition:addDamage(5, 1000, - bleedbear / 3)
-			combat:addCondition(condition)
-			
-		else
-			condition2:addDamage(2, 1000, -(max / 2))
-			condition2:addDamage(3, 1000, -(max / 2) * 2 / 3)
-			--condition2:addDamage(5, 1000, -(max / 2) / 3)
-			combat:addCondition(condition2)
-		end
-
-	return -min, -max -- TODO : Use New Real Formula instead of an %
+	if player:getForm() == "bear" or player:getForm() == "plant" then -- se for urso, causa metade do dano
+		applyDot(player, player:getTarget(), "bleeding", 0.2, 6)
+		return -min / 2 , -max / 2
+	else
+		applyDot(player, player:getTarget(), "bleeding", 0.5, 6)
+		return -min, -max
+	end
 end
 
 combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
@@ -44,7 +30,7 @@ combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, var)
-	if (creature:getStorageValue(Storage_.metamorfose) > 0) then -- se tiver transformado
+	if player:getForm() then -- se tiver transformado
 		if creature:getPlayer():getFreeHands() then
 			return combat:execute(creature, var)
 		else
@@ -67,7 +53,7 @@ spell:mana(0)
 spell:isPremium(true)
 spell:needDirection(true)
 spell:needWeapon(false)
-spell:cooldown(1 * 1000)
+spell:cooldown(5 * 1000)
 spell:groupCooldown(2 * 1000)
 spell:needLearn(false)
 spell:vocation("druid;true", "elder druid;true")

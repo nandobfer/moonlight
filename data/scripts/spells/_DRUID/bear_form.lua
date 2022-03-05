3d9836a77f9e40d86c7ebebc56d565dc15175647
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-03-05 03:04:05 -0300

    Update bear_form.lua
    now taunts enemies around player every second

--- data/scripts/spells/_DRUID/bear_form.lua
+++ data/scripts/spells/_DRUID/bear_form.lua
@@ -1,5 +1,23 @@
 local spell = Spell("instant")
 
+-- aura que taunta a cada 1 segundo
+local combat_taunt = Combat()
+combat_taunt:setArea(createCombatArea(AREA_SQUARE1X1))
+function onTargetCreature(creature, target)
+	return doChallengeCreature(creature, target)
+end
+combat_taunt:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
+
+local function taunt(creatureid, variant)
+	if Creature(creatureid):getForm() ~= "bear" and Creature(creatureid):getForm() ~= "plant" then
+		return false
+	else
+		addEvent(taunt, 1000, creatureid, variant)
+		return combat_taunt:execute(Creature(creatureid), variant)
+	end
+end
+-- fim da aura
+
 function spell.onCastSpell(creature, variant)
 	local player = creature:getPlayer()
     creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
@@ -14,6 +32,7 @@ function spell.onCastSpell(creature, variant)
 	 -- TRANSFORMAR --
 	if form ~= "bear" then
 		player:addForm("werebear", 300) -- (monstro, % bonus de fist)
+		taunt(player:getId(), variant)
 	else end
 	
     return true
@@ -28,7 +47,8 @@ spell:cooldown(0 * 1000)
 spell:groupCooldown(0 * 1000)
 spell:level(20)
 spell:mana(0)
-spell:hasParams(true)
+spell:hasParams(false)
+spell:isSelfTarget(true)
 spell:isAggressive(false)
 spell:needLearn(false)
 spell:register()

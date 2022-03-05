fd58b0694b02a4bd15cb3e64d46c7fc5b478c8fe
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-03-04 21:19:29 -0300

    Update scarlet_vial_strong.lua

--- data/scripts/spells/healing/scarlet_vial_strong.lua
+++ data/scripts/spells/healing/scarlet_vial_strong.lua
@@ -2,15 +2,15 @@ local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
 	local player = creature:getPlayer()
-	if player:getItemCount(3588) >= 25 and player:getItemCount(236) >= 5 then -- busca 25 blueberry e 5 empty flask
+	if player:getItemCount(3588) >= 25 and player:getItemCount(284) >= 5 then -- busca 25 blueberry e 5 empty flask
 		player:removeItem(3588, 25) -- remove 25 blueberry
-		player:removeItem(236, 4) -- remove 4 flask
+		player:removeItem(284, 4) -- remove 4 flask
 		return creature:conjureItem(283, 236, 5, CONST_ME_MAGIC_GREEN) -- transforma flask em potion
 	else
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		--creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,
-		"Para criar uma pocao de vida, voce precisa ter 5 blueberries que vao fermentar e um frasco vazio pra armazenar! ")
+		"Para criar uma pocao de vida, voce precisa ter 25 blueberries que vao fermentar e 4 frascos vazios pra armazenar! ")
 		return false
 	end
 end

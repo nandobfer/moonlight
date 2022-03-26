caab088c5fb53c25f6e83a0a46296c0d7c35e990
nandobfer <nandobfer@yahoo.com.br>
2022-03-26 12:00:00 -0300

    Update arenaCheck_talk.lua

--- data/scripts/gladiators_arena/arenaCheck_talk.lua
+++ data/scripts/gladiators_arena/arenaCheck_talk.lua
@@ -2,6 +2,15 @@ local arenaCheck = TalkAction("/arenacheck")
 
 function arenaCheck.onSay(player, words, param)
 	arenaCheck()
+
+	creature:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
+	creature:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
+	creature:say("Storage 1000000003: "..Game.getStorageValue(1000000003).."")
+
+	creature:say("Storage 1000000051: "..Game.getStorageValue(1000000051).."")
+	creature:say("Storage 1000000052: "..Game.getStorageValue(1000000052).."")
+	creature:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
+	creature:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
 	return false
 end
 

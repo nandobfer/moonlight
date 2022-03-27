166406166e36e2cecbc15628160101e18935fb0f
nandobfer <nandobfer@yahoo.com.br>
2022-03-26 22:52:58 -0300

    Update arenaCheck_talk.lua

--- data/scripts/gladiators_arena/arenaCheck_talk.lua
+++ data/scripts/gladiators_arena/arenaCheck_talk.lua
@@ -3,14 +3,14 @@ local arenaCheck = TalkAction("/arenacheck")
 function arenaCheck.onSay(player, words, param)
 	arenaCheck()
 
-	creature:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
-	creature:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
-	creature:say("Storage 1000000003: "..Game.getStorageValue(1000000003).."")
+	player:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
+	player:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
+	player:say("Storage 1000000003: "..Game.getStorageValue(1000000003).."")
 
-	creature:say("Storage 1000000051: "..Game.getStorageValue(1000000051).."")
-	creature:say("Storage 1000000052: "..Game.getStorageValue(1000000052).."")
-	creature:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
-	creature:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
+	player:say("Storage 1000000051: "..Game.getStorageValue(1000000051).."")
+	player:say("Storage 1000000052: "..Game.getStorageValue(1000000052).."")
+	player:say("Storage 1000000001: "..Game.getStorageValue(1000000001).."")
+	player:say("Storage 1000000002: "..Game.getStorageValue(1000000002).."")
 	return false
 end
 

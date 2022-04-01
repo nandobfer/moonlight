2e4800a4dd13eccbb1d8d6d9549885e66f74ded4
Pozzani <vipzsoares@gmail.com>
2022-04-01 12:43:14 -0300

    Update arena_reset.lua

--- data/scripts/gladiators_arena/arena_reset.lua
+++ data/scripts/gladiators_arena/arena_reset.lua
@@ -17,130 +17,91 @@ if player:getStorageValue(100000025) ~= nill and player:getStorageValue(10000002
 
     player:setStorageValue(100000025, 0)
 
---Função para calculo da Experience a remover
-    local function getExpForLevel(level)
-    level = level - 1
-    return ((50 * level * level * level) - (150 * level * level) + (400 * level)) / 3
-    end
-
--- Função para resetar o level do player
-    if player:getStorageValue(100000014) < 100 then
-    player:removeExperience(player:getExperience() - getExpForLevel(player:getStorageValue(100000014)), false)
-   else
-    targetLevel = player:getStorageValue(100000014)
-        targetExp = getExpForLevel(targetLevel)
-        addExp = targetExp - player:getExperience()
-        player:addExperience(addExp, false)
-end
-
---Função para resetar o HP do Player
-    player:setMaxHealth(player:getStorageValue(100000001))
-    player:addHealth(player:getMaxHealth())
-
---Função para resetar a Mana do Player
-    player:setMaxMana(player:getStorageValue(100000002))
-    player:addMana(player:getMaxMana())
-
---Reseta vocação do player
-    player:setVocation(player:getStorageValue(100000003))
-
---Reseta Outfit do player
-    player:setOutfit({lookType = (player:getStorageValue(100000004)),lookHead = (player:getStorageValue(100000005)),
-    lookBody = (player:getStorageValue(100000006)),lookLegs = (player:getStorageValue(100000007)),
-    lookFeet = (player:getStorageValue(100000008)),lookAddons = (player:getStorageValue(100000009))})
-  
---Reseta Skill (AXE) do player / reseta progresso do Fist
-    player:setSkillLevel(SKILL_AXE, player:getStorageValue(100000027))
-    player:addSkillTries(SKILL_AXE, player:getStorageValue(100000028))
-
---Reseta Skill (SHIELD) do player / reseta progresso do Fist
-    player:setSkillLevel(SKILL_SHIELD, player:getStorageValue(100000010))
-    player:addSkillTries(SKILL_SHIELD, player:getStorageValue(100000011))
-
-
---Reseta ML do Player / Reseta Progresso do ML 
-    player:setMagicLevel(player:getStorageValue(100000012))
-    player:addManaSpent(player:getStorageValue(100000013))
-
---Identifica o Iten Equipado / Remove o Iten / Repoe o Iten se salvo na Storage
---helmet
-local headItem = player:getSlotItem(CONST_SLOT_HEAD)
-if headItem then
-player:removeItem(headItem.itemid, 1)
-player:addItem(player:getStorageValue(100000016))
-end
-
---armor
-local armorItem = player:getSlotItem(CONST_SLOT_ARMOR)
-if armorItem then
-player:removeItem(armorItem.itemid, 1)
-player:addItem(player:getStorageValue(100000017))
-end
-
---legs
-local legsItem = player:getSlotItem(CONST_SLOT_LEGS)
-if legsItem then
-player:removeItem(legsItem.itemid, 1)
-player:addItem(player:getStorageValue(100000018))
-end
-
---boots
-local feetItem = player:getSlotItem(CONST_SLOT_FEET)
-if feetItem then
-player:removeItem(feetItem.itemid, 1)
-player:addItem(player:getStorageValue(100000019))
-end
-
---Hand
-local leftItem = player:getSlotItem(CONST_SLOT_LEFT)
-if leftItem then
-player:removeItem(leftItem.itemid, 1)
-player:addItem(player:getStorageValue(100000020))
-end
-
---Shield
-local shieldItem = player:getSlotItem(CONST_SLOT_RIGHT)
-if shieldItem then
-player:removeItem(shieldItem.itemid, 1)
-player:addItem(player:getStorageValue(100000021))
-end
-
---Amulet
-local amuletItem = player:getSlotItem(CONST_SLOT_NECKLACE)
-if amuletItem then
-player:removeItem(amuletItem.itemid, 1)
-player:addItem(player:getStorageValue(100000022))
-end
-
---Ring
-local ringItem = player:getSlotItem(CONST_SLOT_RING)
-if ringItem then
-player:removeItem(ringItem.itemid, 1)
-player:addItem(player:getStorageValue(100000023))
-end
-
-
-
 --Cure Burning
 player:removeCondition(CONDITION_FIRE)
 
+    --Remove Skill Condition
+    player:removeCondition(CONDITION_ATTRIBUTES)
+    --Remove Outfit Condition
+    player:removeCondition(CONDITION_OUTFIT)
+    --Reset Arena Storage
+    player:setStorageValue(Storage.GladiatorArena.Arena, 0)
+    --Reset Vocation
+    player:setVocation(player:getStorageValue(Storage.GladiatorArena.OldVocation))
+    
+    --Reset Level
+                --Função para calculo de exp pro proximo level
+            local function getExpForLevel(level)
+            level = level - 1
+            return ((50 * level * level * level) - (150 * level * level) + (400 * level)) / 3
+            end
 
+    if player:getLevel() < player:getStorageValue(Storage.GladiatorArena.Level) then
+    targetLevel = player:getStorageValue(Storage.GladiatorArena.Level)
+    targetExp = getExpForLevel(targetLevel)
+    addExp = targetExp - player:getExperience()
+    player:addExperience(addExp, false)
+    else
+    player:removeExperience(player:getExperience() - getExpForLevel(player:getStorageValue(Storage.GladiatorArena.Level)), false)
+    end
+
+    --Remove item For (teste)
+    local ItemSlot = {
+        CONST_SLOT_HEAD,
+        CONST_SLOT_ARMOR,
+        CONST_SLOT_LEGS,
+        CONST_SLOT_FEET,
+        CONST_SLOT_LEFT,
+        CONST_SLOT_RIGHT,
+        CONST_SLOT_NECKLACE,
+        CONST_SLOT_RING
+        }
+
+    local OldItem = {
+        100000029,
+        100000030,
+        100000031,
+        100000032,
+        100000033,
+        100000034,
+        100000035,
+        100000036
+        }
+
+    for index = 1, #ItemSlot do
+        
+        if player:getSlotItem(ItemSlot[index]) ~= nill and player:getSlotItem(ItemSlot[index]) ~= 0 then
+            local item_id = player:getSlotItem(ItemSlot[index]).itemid
+            player:removeItem(item_id, 1)
+            player:addItem(player:getStorageValue(OldItem[index]))
+        else
+            player:addItem(player:getStorageValue(OldItem[index]))
+        end
+
+        
+    end
+
+
+--Aciona animação do Teleport
+player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+
+--Chama Outro Player de volta da Arena
+if Game.getStorageValue(ArenaStatus[1]) == 1 then
+    Game.setStorageValue(ArenaStatus[1], 0)
+    Game.setStorageValue(ArenaPlayers[1], 0)
+    Game.setStorageValue(ArenaPlayers[2], 0)
 local specs, spec = Game.getSpectators(Position(32087, 32340, 7), false, false, 8, 8, 8, 8)
 	for i = 1, #specs do
 		spec = specs[i]
 		if spec:isPlayer() then
 			spec:teleportTo(Position(32369, 32241, 7))
 			spec:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			spec:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You Lose')
+			spec:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You Win')
 		elseif spec:isMonster() then
 			spec:remove()
 		end
 	end
-
---Aciona animação do Teleport
-    Game.setStorageValue(100000026, 0)
-    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-
+end
 
 end			
 end

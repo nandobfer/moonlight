1d8d69e7c59f075b9b90e7ab6ce9400a145bdc8e
Pozzani <vipzsoares@gmail.com>
2022-04-07 22:08:32 -0300

    Update test_talk.lua

--- data/scripts/gladiators_arena/test_talk.lua
+++ data/scripts/gladiators_arena/test_talk.lua
@@ -2,21 +2,7 @@ local test = TalkAction("/test")
 
 function test.onSay(player, words, param)
 
-    for index2 = 1, #ItemSlot do
-    local voc = player:getStorageValue(Storage.GladiatorArena.NewVocation)
-    local newItem = ArenaVocations[voc][index2]
-        
-        if player:getSlotItem(ItemSlot[index2]) ~= nil and player:getSlotItem(ItemSlot[index2]) ~= 0 then
-            local item_id = player:getSlotItem(ItemSlot[index2]).itemid
-            player:setStorageValue(StorageIndex[index2], item_id)
-            player:removeItem(item_id, 1)
-                        
-        else
-            player:setStorageValue(StorageIndex[index2], 0)
-        end
-            player:addItem(newItem)
-
-    end
+    --teste
 
 end
 test:separator(" ")
